8a9133142764589a94cbf97413c8504bdeaa087a
aniki219 <aniki219@gmail.com>
2024-02-07 16:48:14 -0800

    feedback fixes

--- chapter4/e2-recursion.js
+++ chapter4/e2-recursion.js
@@ -1,6 +1,17 @@
 function isEven(n) {
   if (n < 0) return isEven(-n);
-  if (n == 0) return "Even";
-  if (n == 1) return "Odd";
+  if (n === 0) return true;
+  if (n === 1) return false;
   return isEven(n - 2);
 }
+
+console.log(isEven(50));
+// true
+console.log(isEven(75));
+// false
+console.log(isEven(-1));
+// false
+console.log(isEven(-10));
+// true
+console.log(isEven(0));
+// true
