31f7060eceb46492f9326a43524e505a6515ff35
aniki219 <aniki219@gmail.com>
2024-03-11 20:17:23 -0700

    Revert "initial commit"
    This reverts commit 30ab6cdebb2273e9cb28c969fe1ba194e0e33db8.

--- Exercises/chapter3/e3-chessboard.js
+++ Exercises/chapter3/e3-chessboard.js
@@ -2,7 +2,7 @@ let size = 8;
 let board = "";
 for (let row = 0; row < size; row++) {
   for (let col = 0; col < size; col++) {
-  	board += ((row + col) % 2 ==== 0) ? " " : "#";
+  	board += ((row + col) % 2 === 0) ? " " : "#";
   }
   board += "\n";
 }
